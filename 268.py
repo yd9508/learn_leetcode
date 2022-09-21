fda2e3eb9fa7f55c5beea6d44c8f16c25ef60c70
yding1995@gmail.com <yd9508>
2022-09-20 23:52:16 -0400

    Create 268.py

---
+++ 268.py
@@ -0,0 +1,12 @@
+"""
+268. Missing Number
+Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+"""
+class Solution:
+    def missingNumber(self, nums):
+        nums.sort()
+        for i in range(len(nums)):
+            if i != nums[i]:
+                return i
+
+        return len(nums)
