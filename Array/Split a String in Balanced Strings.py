d6a5d6ba9258e89171ce24be343bee2c9f560445
yding1995 <yding1995@gmail.com>
2022-11-23 20:28:40 -0500

    Update Split a String in Balanced Strings.py

--- Array/Split a String in Balanced Strings.py
+++ Array/Split a String in Balanced Strings.py
@@ -14,6 +14,36 @@ class Solution:
         if len(s) == 2:
             return 1 if s[1] != s[0] else 0
 
+        res = 0
+
+        head = 0
+        tail = 1
+
+        while head < len(s):
+            if self.isBalanceStr(s[head:(tail + 1)]):
+                res  = res + 1
+                head = tail + 1
+                tail = tail + 2
+            else:
+                if tail < len(s):
+                    tail = tail + 1
+                else:
+                    return res
+
+        return res
+
+
+    def isBalanceStr(self, s):
+        dic = {}
+        dic["L"] = 0
+        dic["R"] = 0
         for i in range(len(s)):
+            if s[i] == "L":
+                dic["L"] = dic["L"] + 1
+            else:
+                dic["R"] = dic["R"] + 1
 
-
+        if dic["R"] == dic["L"]:
+            return True
+        else:
+            return False
